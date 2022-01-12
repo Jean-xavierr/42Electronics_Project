ba59a6899bcedda8e15f137d6a517325e3716002
Jean-xavierr <religieuxjxavier@gmail.com>
2022-01-12 13:59:47 +0100

    :arrow_up: feat: add prog_01/ex01

--- prog_01/ex01/main.c
+++ prog_01/ex01/main.c
@@ -4,11 +4,15 @@ int     main(void)
 {
     DDRB = (1 << PB1);
 
-    TCCR1B |= (1 << WGM12);                 // CTC mode (Clear Timer on Compare - datasheet p.131 & p.141)
-    TCCR1A |= (1 << COM1A0);                // Toggles OC1A pin each cycle through
-    TCCR1B |= (1 << CS12);                  // prescaler (datasheet p.143)
+    TCCR1B |= (1 << WGM12) | (1 << WGM13);  // Fast PWM mode (fast Pulse Width Modulation - mode 14 p.142)
+    TCCR1A |= (1 << WGM11);
+    TCCR1A |= (1 << COM1A1);                // Clear OC1A on Compare Match, set OC1A at BOTTOM (ch16-2 p.140)
+    TCCR1B |= (1 << CS12);                  // prescaler
 
-    OCR1A = F_CPU / 256 / 2;                // Output Compare Registers
+    ICR1 = F_CPU / 256;                     // set pwm frequency (62500)
+    OCR1A = ICR1 / 10;                      // duty cycle 10 percent
+                                            // sawtooth diagram (https://www.mathworks.com/help/sltest/ref/operator_sawtooth.png)
+
     for (;;) {}
     return 0;
 }
