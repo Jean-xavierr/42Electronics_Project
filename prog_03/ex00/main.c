4253344ec5c78d7a2a982d9cfa480ff4de86d81b
Jean-xavierr <religieuxjxavier@gmail.com>
2022-01-13 17:27:08 +0100

    :arrow_up: feat: remove debug prog_03/ex00

--- prog_03/ex00/main.c
+++ prog_03/ex00/main.c
@@ -13,21 +13,19 @@ void    uart_init(uint8_t USART_BAUDRATE)
 	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);    // Use 8-bit char size
 	UBRR0L = USART_BAUDRATE;	                // Set Baudrate                   
 	UBRR0H = (UBRR0L >> 8);		                    
-
 }
 
 void    uart_tx(char c)
 {
 	while (!(UCSR0A & (1 << UDRE0)));	       // Wait for empty transmit buffer
 	UDR0 = c;
-    DDRB |= (1 << PD3);
-    PORTB ^= (1 << PB3);
 }
 
-int main()
+int     main(void)
 {
 	uart_init(8);                               // 115.2k
     uart_tx('Z');
 	for (;;) {
-    }	
+    }
+    return 0;
 }
