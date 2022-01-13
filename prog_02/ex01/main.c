f5744e305a960bde1bd69c6473e1e5bbffc48185
Jean-xavierr <religieuxjxavier@gmail.com>
2022-01-13 14:24:03 +0100

    :arrow_up: feat: add prog_02/ex01

--- prog_02/ex01/main.c
+++ prog_02/ex01/main.c
@@ -2,14 +2,16 @@
 #include <avr/interrupt.h>
 typedef unsigned char uint8_t;
 
-uint8_t ratio = 0;
+uint8_t ratio = 1;
 uint8_t status = 1;
 
-void    change_status(void)
+void    TIMER0_COMPA_vect(void)
 {
     if (ratio == 100 || ratio == 0)
         status *= -1;
     ratio += status;
+    OCR1A = ICR1 / ratio; 
+    SREG |= (1 << 7);
 }
 
 int     main(void)
@@ -21,20 +23,21 @@ int     main(void)
     TCCR1B |= (1 << WGM12) | (1 << WGM13);  // Fast PWM mode (fast Pulse Width Modulation - mode 14 p.142)
     TCCR1A |= (1 << WGM11);
     TCCR1A |= (1 << COM1A1);                // Clear OC1A on Compare Match, set OC1A at BOTTOM (ch16-2 p.140)
-    TCCR1B |= (1 << CS12);                  // prescaler
+    TCCR1B |= (1 << CS10) | (1 << CS11);                  // prescaler
 
-    ICR1 = F_CPU / 256;                     // set pwm frequency (62500)
-    OCR1A = ICR1 / ratio;                      // duty cycle 10 percent
+    ICR1 = F_CPU / 64 / 100;               // set pwm frequency
+    OCR1A = ICR1 / ratio;                   // duty cycle percent
                                             // sawtooth diagram (https://www.mathworks.com/help/sltest/ref/operator_sawtooth.png)
 
     // TIMER0
 
     TCCR0A |= (1 << WGM01);                 // CTC mode
     TCCR0A |= (1 << COM0A1);                
-    TCCR0B |= (1 << CS02);                  // prescaler   
+    TCCR0B |= (1 << CS00) | (1 << CS02);    // prescaler   
 
-    OCR0A = 255; 
-    // TIMSK0 = (1 << TOIE0);
+    OCR0A = 100; 
+    TIMSK0 = (1 << OCIE0A);
+    SREG = (1 << 7);
     for (;;) {}
     return 0;
 }
