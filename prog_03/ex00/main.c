ad76e1e2e7e9c02d6bcf071e84362bbf74f047c8
Jean-xavierr <religieuxjxavier@gmail.com>
2022-01-13 17:10:22 +0100

    :arrow_up: feat : add prog_03/ex01

---
+++ prog_03/ex00/main.c
@@ -0,0 +1,33 @@
+#ifndef __AVR_ATmega328P
+#define __AVR_ATmega328P
+#endif
+
+#include <avr/io.h>
+
+// Using screen to Connect to the Serial Console
+// screen /dev/<console_port> 115200
+
+void    uart_init(uint8_t USART_BAUDRATE)
+{
+    UCSR0B |= (1 << TXEN0);	                    // Turn on transmission
+	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);    // Use 8-bit char size
+	UBRR0L = USART_BAUDRATE;	                // Set Baudrate                   
+	UBRR0H = (UBRR0L >> 8);		                    
+
+}
+
+void    uart_tx(char c)
+{
+	while (!(UCSR0A & (1 << UDRE0)));	       // Wait for empty transmit buffer
+	UDR0 = c;
+    DDRB |= (1 << PD3);
+    PORTB ^= (1 << PB3);
+}
+
+int main()
+{
+	uart_init(8);                               // 115.2k
+    uart_tx('Z');
+	for (;;) {
+    }	
+}
